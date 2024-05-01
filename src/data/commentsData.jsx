d8091b5e9f0cacfccf2ed7a19dd7f32bb459d357
Manikandan <168514765+KaviMani09@users.noreply.github.com>
2024-05-01 12:28:49 +0530

    Add files via upload

---
+++ src/data/commentsData.jsx
@@ -0,0 +1,25 @@
+const commentsData = [
+    {
+      id: 1,
+      userId: 1,
+      name: "Mugundhan",
+      contentPic:
+        "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        profilePic: "https://i.postimg.cc/mZP646wP/2289-Sk-VNQSBGQU1-PIDEw-Mjgt-MTIy.jpg",
+      desc: "Nice.",
+    },
+    {
+      id: 2,
+      userId: 2,
+      name: "karthi",
+      contentPic:
+        "https://images.pexels.com/photos/13916255/pexels-photo-13916255.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        profilePic: "https://i.postimg.cc/mZP646wP/2289-Sk-VNQSBGQU1-PIDEw-Mjgt-MTIy.jpg",
+      desc: "Super Bro.",
+    },
+   
+    
+  ];
+  
+  export default commentsData;
+  
